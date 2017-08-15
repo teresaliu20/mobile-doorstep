1712646dab38f04db50bd0bcd2ebe47e5df9733d
teresaliu20 <teresali@usc.edu>
2017-08-14 17:03:48 -0700

    initial build

---
+++ index.ios.js
@@ -0,0 +1,9 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import { AppRegistry } from 'react-native';
+import mobileDoorstep from './app/app';
+
+AppRegistry.registerComponent('mobileDoorstep', () => mobileDoorstep);
